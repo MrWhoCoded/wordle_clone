75b08e4ee8dbc13f2454132b73b10b4d7a7afba1
swarup-nandakumar <83503229+swarup-nandakumar@users.noreply.github.com>
2022-11-10 00:50:20 -0800

    Add files via upload

--- dict_words_filter.py
+++ dict_words_filter.py
@@ -2,22 +2,28 @@ import json,pickle
 
 #to extract data from file.
 all_words_cleaned ={}
-all_words = json.load(open("dictionary_of_words.json", "r"))
+all_words = json.load(open("D:/12E yasser and swarup project/worlde_clone-main (1)/worlde_clone-main/dictionary_of_words.json", "r"))
+
+
 all_words_items = all_words.items()#to prevent due to changing dictionary size during iteration.(?)
 
 for word,meaning in all_words_items:
     if len(word)  in range(3,8): # to check if the words are not of length 3-7.
         pass
-    if not word.isupper():
+    elif not word.isupper():
         pass
-    if not word.isspace():
+    elif not word.isspace():
         pass
-    if not word.isalpha():
-        pass
-    else:          
-        all_words_cleaned[word]=meaning 
+    elif  word.isalpha():
+        print(word)
+        all_words_cleaned[word]= meaning 
+    else:
+        continue
 
+print("yessir1")
+print (all_words_cleaned)
 
+quit()
 #rewriting edition dictionary in a new binary file.
 with open("dictionary_of_words_cleaned.txt", "wb") as new_dict:
          pickle.dump(all_words_cleaned,new_dict)
